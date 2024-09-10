c40d5eb5bfeec9dfca66a5b2cc7a2df7ee2a6da0
Danniel0663 <164428344+Danniel0663@users.noreply.github.com>
2024-09-09 21:19:09 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const servicios = document.querySelectorAll('.imagen-port');
+
+    servicios.forEach(servicio => {
+        servicio.addEventListener('click', function() {
+            const info = this.querySelector('.servicio-info');
+            info.style.display = info.style.display === 'block' ? 'none' : 'block';
+        });
+    });
+});
